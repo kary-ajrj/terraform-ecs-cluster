94dc6d2a8a04a0a6630130975b42ddcc0124c79b
Karishma <karishma@technogise.com>
2023-03-22 17:50:15 +0530

    Connectivity to EC2.

--- terraform/ecs-cluster.tf
+++ terraform/ecs-cluster.tf
@@ -1,13 +1,31 @@
 resource "aws_vpc" "ecs_vpc" {
-  cidr_block = "10.0.0.0/24"
-  tags       = {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags                 = {
     Name = "terraform-VPC"
   }
 }
 
-resource "aws_subnet" "ecs_subnet" {
+resource "aws_internet_gateway" "IG" {
+  vpc_id = aws_vpc.ecs_vpc.id
+}
+
+resource "aws_route_table" "routeTable" {
+  vpc_id = aws_vpc.ecs_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IG.id
+  }
+}
+
+resource "aws_route_table_association" "routeTableAssociation" {
+  route_table_id = aws_route_table.routeTable.id
+  subnet_id = aws_subnet.ecs_public_subnet.id
+}
+
+resource "aws_subnet" "ecs_public_subnet" {
   vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = "10.0.0.0/28"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 }
@@ -15,9 +33,16 @@ resource "aws_subnet" "ecs_subnet" {
 resource "aws_security_group" "ecs_security_group" {
   vpc_id = aws_vpc.ecs_vpc.id
   ingress {
-    from_port   = 80
+    from_port   = 443
     protocol    = "tcp"
-    to_port     = 80
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
@@ -74,7 +99,7 @@ resource "aws_autoscaling_group" "auto_scale" {
   max_size             = 1
   min_size             = 1
   launch_configuration = aws_launch_configuration.launch_config.name
-  vpc_zone_identifier  = [aws_subnet.ecs_subnet.id]
+  vpc_zone_identifier  = [aws_subnet.ecs_public_subnet.id]
   #  health_check_type = "EC2"
   desired_capacity     = 1
   termination_policies = [
