23efe13c0e710585cc9d1c65d86f6c0201765824
Karishma <karishma@technogise.com>
2023-04-25 16:15:07 +0530

    Revert "Modularies. Adds LB."
    This reverts commit f1063b6499b44172736dc6ac881f610dea5a3b7f.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,34 @@ resource "aws_ecr_repository" "ecr_example" {
   name = "my_terraform_ecr_example"
 }
 
-resource "aws_vpc" "ecs_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  tags                 = {
-    Name = "terraform-VPC"
+resource "aws_ecs_task_definition" "task_example" {
+  family                = "service"
+  container_definitions = jsonencode([
+    {
+      name              = "hello"
+      image             = aws_ecr_repository.ecr_example.repository_url
+      memoryReservation = 985
+      essential         = true
+      portMappings      = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+}
+
+resource "aws_ecs_service" "service_example" {
+  name            = "ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_example.arn
+  launch_type     = "EC2"
+  desired_count   = 1
+  network_configuration {
+    subnets         = [aws_subnet.ecs_public_subnet.id]
+    security_groups = [aws_security_group.ecs_security_group.id]
   }
 }
