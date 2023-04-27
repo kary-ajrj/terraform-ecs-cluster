6cda4dcef19aae5391e1810f210e150fad4d20fe
Karishma <karishma@technogise.com>
2023-04-27 18:15:45 +0530

    Refactors.

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,3 +1,17 @@
+resource "aws_subnet" "ecs_public_subnet" {
+  vpc_id                  = aws_vpc.ecs_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "ecs_public_subnet_two" {
+  vpc_id                  = aws_vpc.ecs_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
+}
+
 resource "aws_internet_gateway" "IG" {
   vpc_id = aws_vpc.ecs_vpc.id
 }
@@ -18,20 +32,6 @@ resource "aws_route_table_association" "routeTableAssociation" {
 resource "aws_route_table_association" "secondRouteTableAssociation" {
   route_table_id = aws_route_table.routeTable.id
   subnet_id      = aws_subnet.ecs_public_subnet_two.id
-}
-
-resource "aws_subnet" "ecs_public_subnet" {
-  vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "ecs_public_subnet_two" {
-  vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
-  map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "ecs_security_group" {
