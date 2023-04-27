aa7e58807189516c0f752cdea039dee89a1c32cf
Karishma <karishma@technogise.com>
2023-04-27 16:20:40 +0530

    Refactors.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,3 @@ provider "aws" {
 resource "aws_ecr_repository" "ecr_example" {
   name = "my_terraform_ecr_example"
 }
-
-resource "aws_vpc" "ecs_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  tags                 = {
-    Name = "terraform-VPC"
-  }
-}
