2aaecd093dacb377f94341f15737ae003101168f
Karishma <kary-ajrj@users.noreply.github.com>
2023-04-27 18:02:15 +0530

    Ecs cluster task service on UI (#2)
    Co-authored-by: Karishma <karishma@technogise.com>

--- terraform/ecs-cluster.tf
+++ terraform/ecs-cluster.tf
@@ -6,7 +6,6 @@ resource "aws_launch_configuration" "launch_config" {
   image_id                    = "ami-0c6b5b7ffdb17cb99"
   iam_instance_profile        = aws_iam_instance_profile.ecs_agent.name
   security_groups             = [aws_security_group.ecs_security_group.id]
-  associate_public_ip_address = true
   instance_type               = "t2.micro"
   key_name                    = "ecs-ec2-key-pair"
   user_data                   = <<EOF
